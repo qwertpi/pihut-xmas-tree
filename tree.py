82a81b108e77ef7cfd582dc3b101a1501420ca7a
qwertpi <qwertpi@users.noreply.github.com>
2018-12-10 18:49:36 +0000

    Added comments

--- tree.py
+++ tree.py
@@ -2,12 +2,14 @@ from datetime import datetime
 from random import randint,random
 from gpiozero import LEDBoard
 from time import sleep
+#create an LEDBoard with all the pins for the LEDs on the tree bar the star LED which is controlled by the star code
 tree = LEDBoard(*range(3,28),pwm=True)
-tree.off()
-tree.on()
-sleep(5)
 while True:
+    #picks a random number which will be used as an index for the led we will change
     r=randint(0,len(tree)-1)
+    #store the brightness of the led with the index of r
     old=tree[r].value
+    #change the brightness of the led with the index of r to a random number
     tree[r].value=(random())
+    #sleep for half the diffrence between the old brightness and new brightness as a positive number
     sleep(abs(tree[r].value-old)/2)
