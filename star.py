6ce5f54b942b872d340ace5c4ebbf2ae32ab34bd
qwertpi <qwertpi@users.noreply.github.com>
2018-12-16 11:40:01 +0000

    Fixed typos in comments and added whitespace

--- star.py
+++ star.py
@@ -4,14 +4,18 @@ from gpiozero import PWMLED
 star = PWMLED(2)
 #store christmas day in the current year in the c_day variable
 c_day=datetime(datetime.now().year,12,25)
+
+#subrotuine to advoid copy pasting code
 def get_time():
     #get the current time down to the microsecond
     today = datetime.now()
-    #find the diffrence between c_day and the time in the today variable
+    #find the difference between c_day and the time in the today variable
     rd = c_day-today
-    #converts rd as a number of seconds to a string to so we can get only everything before the decimal point and then converts that to an integer and divides that integer by 500,000
+    #converts rd as a number of seconds to a string to so we can get only everything before the decimal point
+    #then converts that to an integer and divides that integer by 500,000
     time=int(str(rd.total_seconds()).split(".")[0])/500000
     return time
+
 while True:
     #turns the star on
     star.on()
