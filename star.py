227b8d64951bc1f06eb3ab271b7869435fd089b0
qwertpi <qwertpi@users.noreply.github.com>
2018-12-16 11:34:00 +0000

    Recalucates time before turning led off and general code optmisation

--- star.py
+++ star.py
@@ -2,18 +2,22 @@ from datetime import datetime
 from time import sleep
 from gpiozero import PWMLED
 star = PWMLED(2)
-while True:
+#store christmas day in the current year in the c_day variable
+c_day=datetime(datetime.now().year,12,25)
+def get_time():
     #get the current time down to the microsecond
     today = datetime.now()
-    #find the diffrence between christmas day in the current year and the time in the today variable
-    rd = datetime(datetime.now().year,12,25)-today
-    #converts rd as a number of seconds to a string to so we can get only everyhting before the decimal point and then converts that to an integer and divides that integer by 500,000
+    #find the diffrence between c_day and the time in the today variable
+    rd = c_day-today
+    #converts rd as a number of seconds to a string to so we can get only everything before the decimal point and then converts that to an integer and divides that integer by 500,000
     time=int(str(rd.total_seconds()).split(".")[0])/500000
+    return time
+while True:
     #turns the star on
     star.on()
-    #waits for the amount of seconds in time
-    sleep(time)
+    #waits for the amount of seconds returned by get_time
+    sleep(get_time())
     #turns the star off
     star.off()
-    #waits for the amount of seconds in time
-    sleep(time)
+    #waits for the amount of seconds returned by get_time
+    sleep(get_time())
